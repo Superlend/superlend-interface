8ffb25c6f229cb862a5f9aa2b8aa101a1b3d5164
shreyaspangal <shreyaspangal03@gmail.com>
2024-09-16 17:36:05 +0530

    add: middleware for default route to /home

---
+++ middleware.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request: any) {
+    const url = request.nextUrl.clone();
+    if (url.pathname === '/') {
+        url.pathname = '/home'; // Set your desired default route here
+        return NextResponse.redirect(url);
+    }
+    return NextResponse.next();
+}
