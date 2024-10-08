f0d3eb5f23b99d522a8fd4f4ac3d0f43084b28ca
shreyaspangal <shreyaspangal03@gmail.com>
2024-10-08 15:53:10 +0530

    add: asset history graph filters drawer UI

--- components/dropdowns/RadioGroupDropdown.tsx
+++ components/dropdowns/RadioGroupDropdown.tsx
@@ -13,6 +13,8 @@ import {
 import {
     Drawer,
     DrawerContent,
+    DrawerHeader,
+    DrawerTitle,
     DrawerTrigger,
 } from "@/components/ui/drawer"
 import { Button } from '../ui/button';
@@ -24,7 +26,14 @@ export default function RadioGroupDropdown({ triggerLabel, listData, defaultValu
     const { width: screenWidth } = useDimensions();
     const isDesktop = screenWidth > 768;
 
-    // if (isDesktop) {
+    function handleValueChange(value: any) {
+        return () => {
+            onValueChange(value);
+            setIsOpen(false);
+        }
+    }
+
+    if (isDesktop) {
         return (
             <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
                 <DropdownMenuTrigger asChild>
@@ -44,29 +53,32 @@ export default function RadioGroupDropdown({ triggerLabel, listData, defaultValu
                 </DropdownMenuContent>
             </DropdownMenu>
         )
-    // }
+    }
 
-    // return (
-    //     <Drawer open={isOpen} onOpenChange={setIsOpen}>
-    //         <DrawerTrigger asChild>
-    //             <Button size="lg" className="w-fit data-[state=open]:ring-2 data-[state=open]:ring-secondary-500 text-gray-800">
-    //                 {value.label || "Filter by"}
-    //                 <ChevronDownIcon className={`w-4 h-4 ml-2 transition-all duration-300 ${isOpen ? 'rotate-180' : ''}`} />
-    //             </Button>
-    //         </DrawerTrigger>
-    //         <DrawerContent>
-    //             <ul className="flex flex-col overflow-y-auto pb-8">
-    //                 {listData.map((item: any) => (
-    //                     <li 
-    //                     key={item.value} 
-    //                     // onClick={onValueChange}
-    //                     className="cursor-pointer flex items-center gap-2 py-2 px-4 hover:bg-gray-200 rounded-2">
-    //                         <Circle className={`${item.value === value.value ? "h-3 w-3 text-secondary-500 ring-1 ring-secondary-100 ring-offset-2" : "h-4 w-4 text-gray-400"} fill-current rounded-full`} />
-    //                         <span className="">{item.label}</span>
-    //                     </li>
-    //                 ))}
-    //             </ul>
-    //         </DrawerContent>
-    //     </Drawer>
-    // )
+    return (
+        <Drawer open={isOpen} onOpenChange={setIsOpen}>
+            <DrawerTrigger asChild>
+                <Button size="lg" className="w-fit data-[state=open]:ring-2 data-[state=open]:ring-secondary-500 text-gray-800">
+                    {value.label || "Filter by"}
+                    <ChevronDownIcon className={`w-4 h-4 ml-2 transition-all duration-300 ${isOpen ? 'rotate-180' : ''}`} />
+                </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>Filters</DrawerTitle>
+                </DrawerHeader>
+                <ul className="flex flex-col overflow-y-auto pb-8">
+                    {listData.map((item: any) => (
+                        <li
+                            key={item.value}
+                            onClick={handleValueChange(item)}
+                            className="cursor-pointer flex items-center gap-2 py-2 px-4 hover:bg-gray-200 rounded-2">
+                            <Circle className={`${item.value === value.value ? "h-3 w-3 text-secondary-500 ring-1 ring-secondary-100 ring-offset-2" : "h-4 w-4 text-gray-400"} fill-current rounded-full`} />
+                            <span className="">{item.label}</span>
+                        </li>
+                    ))}
+                </ul>
+            </DrawerContent>
+        </Drawer>
+    )
 }
