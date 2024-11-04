71ec3639d47b8e2ce733f5ac5d9005089d5b311e
shreyaspangal <shreyaspangal03@gmail.com>
2024-11-04 20:48:52 +0530

    fix: additional rewards default image fallback

--- data/table/top-apy-opportunities.tsx
+++ data/table/top-apy-opportunities.tsx
@@ -345,7 +345,7 @@ function getRewardsTooltipContent({ baseRateFormatted, rewards, apyCurrent }: { 
     return (
         <div className="flex flex-col divide-y divide-gray-800">
             <BodyText level="body1" weight="medium" className="py-2 text-gray-800/75">Rate & Rewards</BodyText>
-            <div className="flex items-center justify-between gap-[100px] py-2" style={{ gap: '70px' }}>
+            <div className="flex items-center justify-between gap-[70px] py-2" style={{ gap: '70px' }}>
                 <div className="flex items-center gap-1">
                     <ChartNoAxesColumnIncreasing className="w-[16px] h-[16px] text-gray-800" />
                     <Label weight="medium" className="text-gray-800">Base rate</Label>
@@ -358,7 +358,7 @@ function getRewardsTooltipContent({ baseRateFormatted, rewards, apyCurrent }: { 
                 rewards?.map((reward: TReward) => (
                     <div key={reward.asset.address} className="flex items-center justify-between gap-[100px] py-2" style={{ gap: '70px' }}>
                         <div className="flex items-center gap-1">
-                            <ImageWithDefault src={reward.asset.logo} width={16} height={16} className="inline-block rounded-full object-contain" />
+                            <ImageWithDefault src={reward.asset.logo || ""} width={16} height={16} className="inline-block rounded-full object-contain" />
                             <Label weight="medium" className="truncate text-gray-800 max-w-[100px] truncate" title={reward.asset.name}>
                                 {reward.asset.name}
                             </Label>
