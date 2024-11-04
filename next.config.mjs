d175fef1e846f75372944dc99646284586a219b1
shreyaspangal <shreyaspangal03@gmail.com>
2024-11-04 17:12:39 +0530

    add: domain for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['superlend-assets.s3.ap-south-1.amazonaws.com'],
+    },
+};
 
 export default nextConfig;
