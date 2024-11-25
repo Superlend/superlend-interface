c4b50e2687e1e98993b5f6b7fac61b58443cfbd3
shreyaspangal <shreyaspangal03@gmail.com>
2024-11-25 13:16:48 +0530

    add: discover page prefetch data

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -4,16 +4,16 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import useGetOpportunitiesData from '@/hooks/useGetOpportunitiesData'
 
 export default async function DiscoverPage() {
-    // const queryClient = new QueryClient()
+    const queryClient = new QueryClient()
 
-    // await queryClient.prefetchQuery({
-    //     queryKey: ['opportunities'],
-    //     queryFn: () => useGetOpportunitiesData({ type: "lend" }),
-    // })
+    await queryClient.prefetchQuery({
+        queryKey: ['opportunities'],
+        queryFn: () => useGetOpportunitiesData({ type: "lend" }),
+    })
 
     return (
-        // <HydrationBoundary state={dehydrate(queryClient)}>
+        <HydrationBoundary state={dehydrate(queryClient)}>
             <DiscoverPageComponents />
-        // </HydrationBoundary>
+        </HydrationBoundary>
     )
 }
