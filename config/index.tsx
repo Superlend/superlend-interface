8718ffdaedfe132c05b710f9653cf4f5cccf6978
shreyaspangal <shreyaspangal03@gmail.com>
2025-01-08 12:07:16 +0530

    refactor: setup for projectId is and queryClient defined in context

--- config/index.tsx
+++ config/index.tsx
@@ -15,7 +15,7 @@ import {
 import { defineChain } from '@reown/appkit/networks'
 
 // Get projectId from https://cloud.reown.com
-export const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID || ''
+export const projectId = process.env.NEXT_PUBLIC_REOWN_PROJECT_ID
 
 if (!projectId) {
     throw new Error('Project ID is not defined')
--- context/index.tsx
+++ context/index.tsx
@@ -2,8 +2,8 @@
 
 import { customMetisNetwork, projectId, wagmiAdapter } from '@/config'
 import React, { type ReactNode } from 'react'
-import { queryClient } from './query-client'
-import { QueryClientProvider } from '@tanstack/react-query'
+// import { queryClient } from './query-client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AssetsDataProvider from './data-provider'
 import { WagmiProvider } from 'wagmi'
 import { Config, cookieToInitialState } from 'wagmi'
@@ -21,6 +21,13 @@ import {
     etherlink,
 } from '@reown/appkit/networks'
 import { createAppKit } from '@reown/appkit/react'
+
+// Set up queryClient
+const queryClient = new QueryClient()
+
+if (!projectId) {
+    throw new Error('Project ID is not defined')
+}
 
 // Set up metadata
 const metadata = {
