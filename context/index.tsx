af3e524e30eea3d51c7a124500a52e74ec7d7dd5
shreyaspangal <shreyaspangal03@gmail.com>
2025-01-23 18:08:39 +0530

    add: supported wallets list

--- context/index.tsx
+++ context/index.tsx
@@ -110,6 +110,7 @@ function ContextProvider({
                     landingHeader: 'Connect Wallet',
                     loginMessage: 'Select wallet to continue',
                     showWalletLoginFirst: true,
+                    walletList: ['metamask', 'coinbase_wallet', 'okx_wallet', 'rainbow', 'rabby_wallet', 'phantom', 'uniswap', 'detected_ethereum_wallets', 'wallet_connect']
                 },
                 supportedChains: [mainnet, polygon, base, scroll, avalanche, optimism, bsc, gnosis, arbitrum, etherlink, metis],
             }}
