0cfcf64984a4e1fae175eddce04094c1b929539b
shreyaspangal <shreyaspangal03@gmail.com>
2025-03-06 12:11:42 +0530

    fix: Remove redundant condition in fluid transaction widget text display

--- app/position-management/tx-widgets/fluid-tx-widget.tsx
+++ app/position-management/tx-widgets/fluid-tx-widget.tsx
@@ -880,7 +880,6 @@ function FluidVaults({
                                     className="mx-auto w-full text-gray-500 text-center max-w-[250px]"
                                 >
                                     {isLendPositionType(positionType) &&
-                                        positionTypeParam === 'borrow' &&
                                         'Enter amount to proceed with supplying collateral for this position'}
                                     {!isLendPositionType(positionType) &&
                                         doesMarketHasLiquidity &&
