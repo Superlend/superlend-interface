788bcdb8bf5c72962230570cfd7d9ccbe2b48ac5
shreyaspangal <shreyaspangal03@gmail.com>
2025-05-02 15:44:44 +0530

    refactor: update tooltip icons and styles in rewards section

--- data/table/top-apy-opportunities.tsx
+++ data/table/top-apy-opportunities.tsx
@@ -24,7 +24,7 @@ import { ChainId } from '@/types/chain'
 import { PlatformType } from '@/types/platform'
 import { ColumnDef } from '@tanstack/react-table'
 import { motion } from 'framer-motion'
-import { ChartNoAxesColumnIncreasing, ShieldAlertIcon } from 'lucide-react'
+import { ChartNoAxesColumnIncreasing, Percent, ShieldAlertIcon, TrendingUp } from 'lucide-react'
 import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 
@@ -840,7 +840,7 @@ function getRewardsTooltipContent({
             <BodyText
                 level="body1"
                 weight="medium"
-                className="py-2 text-gray-800/75"
+                className="py-2 text-gray-800"
             >
                 Rate & Rewards
             </BodyText>
@@ -849,7 +849,7 @@ function getRewardsTooltipContent({
                 style={{ gap: '70px' }}
             >
                 <div className="flex items-center gap-1">
-                    <ChartNoAxesColumnIncreasing className="w-[16px] h-[16px] text-gray-800" />
+                    <Percent className="w-[14px] h-[14px] text-gray-800" />
                     <Label weight="medium" className="text-gray-800">
                         Base rate
                     </Label>
@@ -871,8 +871,8 @@ function getRewardsTooltipContent({
                     <div className="flex items-center gap-1">
                         <ImageWithDefault
                             src={reward?.asset?.logo || ''}
-                            width={16}
-                            height={16}
+                            width={14}
+                            height={14}
                             alt={reward?.asset?.name || ''}
                             className="inline-block rounded-full object-contain"
                         />
@@ -899,13 +899,7 @@ function getRewardsTooltipContent({
                 style={{ gap: '70px' }}
             >
                 <div className="flex items-center gap-1">
-                    <ImageWithDefault
-                        src={netApyIcon || '/icons/sparkles.svg'}
-                        alt="Net APY"
-                        width={16}
-                        height={16}
-                        className="inline-block"
-                    />
+                    <TrendingUp className="w-[14px] h-[14px] text-gray-800" />
                     <Label weight="medium" className="text-gray-800">
                         Net APY
                     </Label>
