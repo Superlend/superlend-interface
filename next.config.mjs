61f5ea03756b34768ad148106ff479e46f01567b
Vijay Pushkin <a.vijaypushkin@gmail.com>
2025-08-21 12:44:47 +0530

    chore: update image configuration with minimumCacheTTL for various hosts
    - This is solve the high Image Optimization utilization on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,58 @@
+const DAYS30_IN_SECONDS = 2592000 // 60 * 60 * 24 * 30
+const DAYS14_IN_SECONDS = 1209600 // 60 * 60 * 24 * 14
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         remotePatterns: [
             {
                 hostname: 'superlend-assets.s3.ap-south-1.amazonaws.com',
+                minimumCacheTTL: DAYS14_IN_SECONDS,
             },
             {
                 hostname: 'coin-images.coingecko.com',
+                minimumCacheTTL: DAYS30_IN_SECONDS,
             },
             {
                 hostname: 'cdn.morpho.org',
+                minimumCacheTTL: DAYS30_IN_SECONDS,
             },
             {
                 hostname: 'www.etherlink.com',
+                minimumCacheTTL: DAYS30_IN_SECONDS,
             },
             {
                 hostname: 'raw.githubusercontent.com',
+                minimumCacheTTL: DAYS30_IN_SECONDS,
             },
             {
                 hostname: 'superlend-public-assets.s3.ap-south-1.amazonaws.com',
+                minimumCacheTTL: DAYS14_IN_SECONDS,
             },
             {
                 hostname: 'cdn.whisk.so',
+                minimumCacheTTL: DAYS30_IN_SECONDS,
             },
             {
                 hostname: 'cryptologos.cc',
+                minimumCacheTTL: DAYS30_IN_SECONDS,
             },
             {
                 hostname: 'funds.superlend.xyz',
+                minimumCacheTTL: DAYS14_IN_SECONDS,
             },
             {
                 hostname: 'app.aave.com',
+                minimumCacheTTL: DAYS30_IN_SECONDS,
             },
         ],
         dangerouslyAllowSVG: true,
         contentDispositionType: 'attachment',
-        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+        contentSecurityPolicy:
+            "default-src 'self'; script-src 'none'; sandbox;",
+        contentSecurityPolicy:
+            "default-src 'self'; script-src 'none'; sandbox;",
+        minimumCacheTTL: DAYS14_IN_SECONDS,
     },
     webpack: (config) => {
         config.resolve.fallback = { fs: false, net: false, tls: false }
